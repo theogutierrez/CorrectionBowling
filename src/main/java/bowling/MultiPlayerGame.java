ca6a919c1ab596fdd839967b27ca4acaab03f3b8
Rémi Bastide <Remi.Bastide@gmail.com>
2018-10-10 11:07:00 +0200

    Comments

--- src/main/java/bowling/MultiPlayerGame.java
+++ src/main/java/bowling/MultiPlayerGame.java
@@ -10,7 +10,7 @@ public interface MultiPlayerGame {
 	 * Démarre une nouvelle partie pour un groupe de joueurs
 	 * @param playerName un tableau des noms de joueurs (il faut au moins un joueur)
 	 * @return une chaîne de caractères indiquant le prochain joueur,
-	 * de la forme "Prochain tir : joueur Bastide, tour n° 5, boule n° 1"
+	 * de la forme "Prochain tir : joueur Bastide, tour n° 1, boule n° 1"
 	 * @throws java.lang.Exception si le tableau est vide ou null
 	 */
 	public String startNewGame(String[] playerName) throws Exception ;
@@ -19,7 +19,7 @@ public interface MultiPlayerGame {
 	 * Enregistre le nombre de quilles abattues pour le joueur courant, dans le frame courant, pour la boule courante
 	 * @param nombreDeQuillesAbattues : nombre de quilles abattue à ce lancer
 	 * @return une chaîne de caractères indiquant le prochain joueur,
-	 * de la forme "Prochain tir : joueur Bastide, tour n° 5, boule n° 1",
+	 * de la forme "Prochain tir : joueur Bastide, tour n° 5, boule n° 2",
 	 * ou bien "Partie terminée" si la partie est terminée.
 	 * @throws java.lang.Exception si la partie n'est pas démarrée, ou si elle est terminée.
 	 */
